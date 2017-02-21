0c7cb16e524cbe03090ffdab0049885c16804ba6
ubu16 <ubu16@ubuDVL-Martinez17>
2017-02-21 17:47:05 +0100

    Versió 0.4-B del projecte prjava03Martinez17

--- src/prjava03Martinez17/Prjava03Martinez17.java
+++ src/prjava03Martinez17/Prjava03Martinez17.java
@@ -2,7 +2,7 @@
 package prjava03Martinez17; 
 import java.awt.*;        // using AWT containers and components 
 import java.awt.event.*;  // using AWT events and listener interfaces 
- 
+import java.net.*;
 // An AWT GUI program inherits the top-level container java.awt.Frame 
 public class Prjava03Martinez17 extends Frame 
       implements ActionListener, WindowListener { 
